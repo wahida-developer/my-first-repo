09f10c70f94106c52ead3dadfbfc58eaf63c176c
Wahida <tbh.wahida@gmail.com>
2025-12-30 18:33:53 +0530

    Day 9: dictionary basics

---
+++ day9.py
@@ -0,0 +1,10 @@
+student = {
+            "name": "Wahida",
+            "age": 17,
+            "course": "Computer Science",
+            "city": "Kanpur"
+}
+print(student)
+print(student["name"])
+print(student["age"])
+print(student["city"])
